6b7167cf63196bb0ab952a608cac16f0059204bb
kebasyaty <kebasyaty@gmail.com>
2025-07-06 12:22:13 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -4,18 +4,6 @@ from typing import Any
 
 from fastapi import FastAPI
 
-from .services.accounts.models import User
+from app.services.accounts.models import User
 
 app = FastAPI()
-
-
-@app.get("/")
-async def read_root() -> dict[str, str]:
-    """???"""
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None) -> dict[str, Any]:
-    """???"""
-    return {"item_id": item_id, "q": q}
