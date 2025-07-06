cb18038f7607b0a8ce07a96bb39d07fd476ee72e
kebasyaty <kebasyaty@gmail.com>
2025-07-06 12:29:26 +0300

    UPDATE main.py

--- app/main.py
+++ app/main.py
@@ -2,4 +2,16 @@
 
 from fastapi import FastAPI
 
+# from pymongo import AsyncMongoClient
+# from ramifice import migration, translations
+
+# client: AsyncMongoClient = AsyncMongoClient()
+
+# await migration.Monitor(
+#     database_name="test_basic",
+#     mongo_client=client,
+# ).migrate()
+
+# await client.close()
+
 app = FastAPI()
