3545d2dc4bb808e1410e5a3a6ffe97a82ef250cd
kebasyaty <kebasyaty@gmail.com>
2025-07-07 11:31:57 +0300

    fix models/user

--- app/services/accounts/models/user.py
+++ app/services/accounts/models/user.py
@@ -35,9 +35,6 @@ class User:
             # Available 4 sizes from lg to xs or None.
             # Hint: By default = None
             thumbnails={"lg": 512, "md": 256, "sm": 128, "xs": 64},
-            # True - high quality and low performance for thumbnails.
-            # Hint: By default = False
-            high_quality=True,
             # The maximum size of the original image in bytes.
             # Hint: By default = 2 MB
             max_size=524288,  # 0.5 MB = 512 KB = 524288 Bytes (in binary)
