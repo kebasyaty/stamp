b76a55fd080da4ada2ba93655bfb55a347fc42cb
kebasyaty <kebasyaty@gmail.com>
2025-07-09 16:44:26 +0300

    update

--- src/app/models.py
+++ src/app/models.py
@@ -4,3 +4,4 @@ Hub of models.
 """
 
 from app.services.accounts.models import *
+from app.services.admin.models import *
--- src/app/services/admin/models/__init__.py
+++ src/app/services/admin/models/__init__.py
@@ -1,1 +1,5 @@
 """App > Services > Admin > Models."""
+
+from app.services.admin.models.admin_panel import AdminPanelParameters
+from app.services.admin.models.company import CompanyParameters
+from app.services.admin.models.site import SiteParameters
