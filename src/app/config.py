184eecadf2913ea32d2a123f0f6b5140d9bed5ef
kebasyaty <kebasyaty@gmail.com>
2025-07-10 09:02:40 +0300

    update

--- src/app/config.py
+++ src/app/config.py
@@ -7,14 +7,14 @@ Global Settings.
 # Production -> False
 DEBUG: bool = True
 
-# Uvicorn Settings.
+# Uvicorn
 UVICORN_APP: str = "app.main:app"
 UVICORN_HOST: str = "127.0.0.1"
 UVICORN_PORT: int = 8000
 UVICORN_RELOAD: bool = DEBUG
 UVICORN_LOG_LEVEL: str = "info"
 
-# MongoDB Settings.
+# MongoDB
 MONGO_HOST: str = "127.0.0.1"
 MONGO_PORT: int = 27017
 MONGO_USERNAME: str | None = None
