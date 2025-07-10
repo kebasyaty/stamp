b079c0b33c965fac1d04047454c629893d1a2d1c
kebasyaty <kebasyaty@gmail.com>
2025-07-10 17:35:39 +0300

    update

--- src/app/config.py
+++ src/app/config.py
@@ -8,11 +8,19 @@ from fastapi.templating import Jinja2Templates
 # Development -> True
 # Production -> False
 DEBUG: bool = True
+# URI Scheme
+URI_SCHEME: str = f"http{'s' if not DEBUG else ''}"
+# URI Host
+URI_HOST: str = "www.your-domain-name.net" if not DEBUG else "127.0.0.1"
+# URI Port
+URI_PORT: int = 5000 if not DEBUG else 8000
+# Application URL
+APP_URL: str = f"{URI_SCHEME}://{URI_HOST}"
 
 # Uvicorn
 UVICORN_APP: str = "app.main:app"
-UVICORN_HOST: str = "127.0.0.1"
-UVICORN_PORT: int = 8000
+UVICORN_HOST: str = URI_HOST
+UVICORN_PORT: int = URI_PORT
 UVICORN_RELOAD: bool = DEBUG
 UVICORN_LOG_LEVEL: str = "info"
 
