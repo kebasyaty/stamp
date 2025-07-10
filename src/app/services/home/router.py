597dc23ec2b31ba655022f91579de273f25d415f
kebasyaty <kebasyaty@gmail.com>
2025-07-10 13:12:30 +0300

    update

--- src/app/services/home/router.py
+++ src/app/services/home/router.py
@@ -3,12 +3,25 @@
 Face of Site.
 """
 
+from typing import Any
+
 from fastapi import APIRouter
+from ramifice import translations
+
+from app.config import TEMPLATES
 
 router = APIRouter(tags=["home"])
 
 
 @router.get("/")
-async def root() -> dict:
+async def root() -> Any:
     """Home Page."""
-    return {"title": "Home Page"}
+    return TEMPLATES.TemplateResponse(
+        name="index.html",
+        context={
+            "lang_code": translations.CURRENT_LOCALE,
+            "meta_title": "Home Page",
+            "meta_description": "???",
+            "message": "Hello World",
+        },
+    )
