451a8c102bdaf65a04f12274e344de25d53b95b5
kebasyaty <kebasyaty@gmail.com>
2025-07-10 13:42:18 +0300

    update

--- src/app/router.py
+++ src/app/router.py
@@ -8,6 +8,7 @@ from typing import Any
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 
+from app.config import STATIC_DIR
 from app.services.accounts.router import router as accounts_router
 from app.services.admin.router import router as admin_router
 from app.services.auth.router import router as auth_router
@@ -20,7 +21,7 @@ root_router.include_router(auth_router)
 root_router.include_router(home_router)
 
 
-@root_router.get("/static/favicons/favicon.ico", include_in_schema=False)
+@root_router.get("/favicon.ico", include_in_schema=False)
 async def get_favicon() -> Any:
     """Get favicon."""
-    return FileResponse("favicon.ico")
+    return FileResponse(f"{STATIC_DIR}/favicons/favicon.ico")
