da3e6aaa9c905f8e728018541dd3a7c06a89fabd
kebasyaty <kebasyaty@gmail.com>
2025-07-28 16:27:10 +0300

    update

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -4,13 +4,14 @@ src > app > utils
 """
 
 import pytest
-from src.app.utils import generate_token
+
+from app.utils import generate_token
 
 
 class TestUtilsClass:
     """Testing utils module."""
 
-    def test_generate_token(self):
+    def test_generate_token(self) -> None:
         """Testing a generate_token method."""
-        token = generate_token(16)
-        assert len(token) == 16
+        token = generate_token(64)
+        assert len(token) == 86
