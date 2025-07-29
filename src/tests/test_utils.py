ff71f5ce80b1ef12cbb620786a5684a0dea7b0f2
kebasyaty <kebasyaty@gmail.com>
2025-07-29 08:31:52 +0300

    add test_get_session_secret_key

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -3,10 +3,23 @@
 src > app > utils
 """
 
-from app.utils import generate_token
+from app.utils import (
+    generate_token,
+    get_session_secret_key,
+)
 
 
 def test_generate_token() -> None:
     """Testing a generate_token method."""
     token = generate_token(64)
+    assert len(token) == 86  # secrets.token_urlsafe(64) -> 86
+
+
+def test_get_session_secret_key() -> None:
+    """Testing a get_session_secret_key method."""
+    token = get_session_secret_key(
+        dotenv_path=".env",
+        length=64,
+    )
+    assert token is not None
     assert len(token) == 86  # secrets.token_urlsafe(64) -> 86
