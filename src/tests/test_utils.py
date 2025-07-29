01ce3954dde2f0c56774148399b89d7a017e17cf
kebasyaty <kebasyaty@gmail.com>
2025-07-29 08:08:36 +0300

    update test_utils

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -6,10 +6,7 @@ src > app > utils
 from app.utils import generate_token
 
 
-class TestUtilsClass:
-    """Testing utils module."""
-
-    def test_generate_token(self) -> None:
-        """Testing a generate_token method."""
-        token = generate_token(64)
-        assert len(token) == 86
+def test_generate_token() -> None:
+    """Testing a generate_token method."""
+    token = generate_token(64)
+    assert len(token) == 86  # secrets.token_urlsafe(64) -> 86
