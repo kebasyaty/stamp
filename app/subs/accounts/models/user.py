0e2fe8a9f957910d081a3184a2579b9f687a7d80
develgen <develgen.dev@gmail.com>
2026-09-10 12:12:09 +0300

    update app/subs/accounts/models/user.py

--- app/subs/accounts/models/user.py
+++ app/subs/accounts/models/user.py
@@ -6,132 +6,147 @@ __all__ = ("User",)
 
 import re
 
-from ramifice import model, to_human_size, translations
-from ramifice.fields import (
-    BooleanField,
-    DateField,
-    DateTimeField,
-    EmailField,
-    ImageField,
-    PasswordField,
-    PhoneField,
-    SlugField,
-    TextField,
+from ramifice import (
+    Model,
+    NamedTuple,
+    Translator,
+    fields,
+    meta,
+    to_human_size,
 )
 
+_ = Translator.STUB_TRANSLATOR_FOR_ATTRIBUTES_OF_FIELD
 
-@model(service_name="Accounts")
-class User:
-    """Model of User."""
 
-    def fields(self) -> None:
-        """For adding fields."""
-        # For custom translations.
-        gettext = translations.gettext
-        self.avatar = ImageField(
-            label=gettext("Avatar"),
-            placeholder=gettext("Upload your photo"),
-            default="public/media/default/no-photo.png",
-            # Directory for images inside media directory.
-            target_dir="users/avatars",
-            # Available 4 sizes from lg to xs or None.
-            # Hint: By default = None
-            thumbnails={"lg": 512, "md": 256, "sm": 128, "xs": 64},
-            # The maximum size of the original image in bytes.
-            # Hint: By default = 2 MB
-            max_size=524288,  # 0.5 MB = 512 KB = 524288 Bytes (in binary)
-            warning=[
-                gettext("Maximum size: {}").format(to_human_size(524288)),
-            ],
-        )
-        self.username = TextField(
-            label=gettext("Username"),
-            placeholder=gettext("Enter your username"),
-            maxlength=150,
-            required=True,
-            unique=True,
-            warning=[
-                gettext("Allowed chars: {}").format("a-z A-Z 0-9 _"),
-            ],
-        )
-        self.first_name = TextField(
-            label=gettext("First name"),
-            placeholder=gettext("Enter your First name"),
-            multi_language=True,  # Support for several language.
-            maxlength=150,
-            required=True,
-        )
-        self.last_name = TextField(
-            label=gettext("Last name"),
-            placeholder=gettext("Enter your Last name"),
-            multi_language=True,  # Support for several language.
-            maxlength=150,
-            required=True,
-        )
-        self.email = EmailField(
-            label=gettext("Email"),
-            placeholder=gettext("Enter your email"),
-            required=True,
-            unique=True,
-        )
-        self.phone = PhoneField(
-            label=gettext("Phone number"),
-            placeholder=gettext("Enter your phone number"),
-            unique=True,
-        )
-        self.birthday = DateField(
-            label=gettext("Birthday"),
-            placeholder=gettext("Enter your date of birth"),
-        )
-        self.password = PasswordField(
-            label=gettext("Password"),
-            placeholder=gettext("Enter your password"),
-        )
-        self.сonfirm_password = PasswordField(
-            label=gettext("Confirm password"),
-            placeholder=gettext("Repeat your password"),
-            # If true, the value of this field is not saved in the database.
-            ignored=True,
-        )
-        self.is_admin = BooleanField(
-            label=gettext("Is Administrator?"),
-            warning=[
-                gettext("Can this user access the admin panel?"),
-            ],
-        )
-        self.is_active = BooleanField(
-            label=gettext("Is active?"),
-            warning=[
-                gettext("Is this an active account?"),
-            ],
-        )
-        self.slug = SlugField(
-            label=gettext("Slug"),
-            slug_sources=["username"],
-            disabled=True,
-            hide=True,
-        )
-        self.last_login = DateTimeField(
-            label=gettext("Last login"),
-            disabled=True,
-            hide=True,
-            warning=[
-                gettext("Date and time of user last login."),
-            ],
-        )
+@meta(service_name="Accounts")
+class User(Model):
+    """User Model."""
+
+    avatar = fields.ImageField(
+        label=_("Avatar"),
+        placeholder=_("Upload your photo"),
+        default="public/media/default/no-photo.png",
+        # Directory for images inside media directory.
+        target_dir="users/avatars",
+        # Available 4 sizes from lg to xs or None.
+        # Hint: By default = None
+        thumbnails={"lg": 512, "md": 256, "sm": 128, "xs": 64},
+        # The maximum size of the original image in bytes.
+        # Hint: By default = 2 MB
+        max_size=524288,  # 512 KB = 0.5 MB = 524288 Bytes (in binary)
+        warning=[
+            _("Maximum size: {}").format(to_human_size(524288)),
+        ],
+    )
+    username = fields.TextField(
+        label=_("Username"),
+        placeholder=_("Enter your username"),
+        max_length=150,
+        is_require=True,
+        is_unique=True,
+        warning=[
+            _("Allowed characters: {}").format("a-z A-Z 0-9 _"),
+            _("Maximum length: {}").format(150),
+        ],
+    )
+    first_name = fields.TextField(
+        label=_("First name"),
+        placeholder=_("Enter your First name"),
+        is_multilingual=True,  # Support for several language.
+        max_length=150,
+        is_require=True,
+        warning=[
+            _("Maximum length: {}").format(150),
+        ],
+    )
+    last_name = fields.TextField(
+        label=_("Last name"),
+        placeholder=_("Enter your Last name"),
+        is_multilingual=True,  # Support for several language.
+        max_length=150,
+        is_require=True,
+        warning=[
+            _("Maximum length: {}").format(150),
+        ],
+    )
+    email = fields.EmailField(
+        label=_("Email"),
+        placeholder=_("Enter your email"),
+        is_require=True,
+        is_unique=True,
+    )
+    phone = fields.PhoneField(
+        label=_("Phone number"),
+        placeholder=_("Enter your phone number"),
+        is_unique=True,
+    )
+    birthday = fields.DateField(
+        label=_("Birthday"),
+        placeholder=_("Enter your date of birth"),
+    )
+    description = fields.TextField(
+        label=_("About yourself"),
+        placeholder=_("Tell us a little about yourself ..."),
+        is_multilingual=True,  # Support for several language.
+    )
+    password = fields.PasswordField(
+        label=_("Password"),
+        placeholder=_("Enter your password"),
+        warning=[
+            _("Maximum length: {}").format(256),  # this is an immutable size
+            _("Minimum length: {}").format(8),  # this is an immutable size
+        ],
+    )
+    confirm_password = fields.PasswordField(
+        label=_("Confirm password"),
+        placeholder=_("Repeat your password"),
+        # If true, the value of this field is not saved in the database.
+        is_ignore=True,
+    )
+    is_admin = fields.BooleanField(
+        label=_("Is Administrator?"),
+        warning=[
+            _("Can this user access the admin panel?"),
+        ],
+    )
+    is_active = fields.BooleanField(
+        label=_("Is active?"),
+        warning=[
+            _("Is this an active account?"),
+        ],
+    )
+    slug = fields.SlugField(
+        label=_("Slug"),
+        slug_sources=["username"],
+        is_disable=True,
+        is_hide=True,
+    )
+    last_login = fields.DateTimeField(
+        label=_("Last login"),
+        is_disable=True,
+        is_hide=True,
+        warning=[
+            _("Date and time of user last login."),
+        ],
+    )
 
     # Optional method
-    async def add_validation(self) -> dict[str, str]:
+    async def add_validation(self) -> NamedTuple:
         """Additional validation of fields."""
-        gettext = translations.gettext
-        cd, err_map = self.get_clean_data()
+        _ = self._CUSTOM_TRANSLATOR.gettext
+        err_map = self.get_error_map()
+
+        username = self.username
+        id = self.id
+        password = self.password
+        confirm_password = self.confirm_password
 
         # Check username
-        if re.match(r"^[a-zA-Z0-9_]+$", cd["username"]) is None:
-            err_map["username"] = gettext("Allowed chars: {}").format("a-z A-Z 0-9 _")
+        if username is not None and re.match(r"^[a-zA-Z0-9_]+$", username) is None:
+            err_map.update("username", _("Allowed characters: {}").format("a-z A-Z 0-9 _"))
 
         # Check password
-        if cd["_id"] is None and (cd["password"] != cd["сonfirm_password"]):
-            err_map["password"] = gettext("Passwords do not match!")
+        if id is None and password != confirm_password:
+            err_map.update("password", _("Passwords do not match!"))
 
         return err_map
